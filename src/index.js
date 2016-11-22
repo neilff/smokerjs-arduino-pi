a28e182cb3833726c48d456e6cb879cc7ef06836
Neil Fenton <neilff@gmail.com>
2016-11-21 22:40:48 -0500

    added basic vanilla output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const five = require('johnny-five');
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const board = new five.Board();
+
+const PORT = process.env.PORT || 3000;
+
+let currentTemp = {};
 
 function convertVoltToTemp(volt) {
   let tempK = 0;
@@ -18,7 +28,6 @@ function convertVoltToTemp(volt) {
   tempF = (tempC * 1.8) + 32;
   tempF = Math.round(tempF * 10) / 10;
 
-  // return all three temperature scales
   return {
     tempK: tempK,
     tempC: tempC,
@@ -30,11 +39,26 @@ const THMPIN = 'A0';
 
 board.on('ready', () => {
   const thm = new five.Sensor({ pin: THMPIN, freq: 500 });
-  let currentTemp = {};
 
   thm.on('change',  (voltage) => {
     currentTemp = convertVoltToTemp(voltage);
-
-    console.log(currentTemp);
+    console.info('currentTemp', currentTemp);
+    io.emit('temperartureUpdate', currentTemp);
   });
+});
+
+app.get('/device/lastReading', function (req, res) {
+  res.json(currentTemp);
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(PORT, function(){
+  console.log(`Listening on port ${ PORT }`);
 });
